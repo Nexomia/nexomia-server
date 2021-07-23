1fadaf8aa52b64a2535a9f62719761db8fa0b6cf
Dmytro <dmytro@d3s.ru>
2021-07-23 17:00:26 +0700

    feat: disable cors validation for dev instance

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 const Fingerprint = require('express-fingerprint');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: process.env.NODE_ENV === 'prod' ? false : true })
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
   app.use(Fingerprint())
