420631c5afcf5a24e4009a3a2e0b8c5de0526852
Dmytro <dmytro@d3s.ru>
2021-07-31 00:04:20 +0700

    fix: unauthorized response everywhere

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,9 +6,10 @@ import { Request, Response, NextFunction } from 'express';
 export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    if (!req.baseUrl.includes('/auth/'))
+    if (!req.baseUrl.includes('/auth/')) {
       req.user = await this.jwtService.decodeAccessToken(req.headers['authorization'])
       if (!req.user) throw new UnauthorizedException()
+    }
     next()
   }
 }
