d7ebb29b8155ff64db5977f24cc4a7002ba6d260
Dmytro <dmytro@d3s.ru>
2021-09-18 21:01:13 +0700

    fix: unknown type error on role parsing

--- src/api/guilds/responses/role.response.ts
+++ src/api/guilds/responses/role.response.ts
@@ -5,7 +5,10 @@ const RoleResponseSchema = myzod.object({
   id: myzod.string(),
   guild_id: myzod.string(),
   name: myzod.string(),
-  permissions: myzod.unknown(),
+  permissions: myzod.object({
+    allow: myzod.number(),
+    deny: myzod.number()
+  }),
   color: myzod.string().optional(),
   hoist: myzod.boolean().optional(),
   position: myzod.number().optional(),
