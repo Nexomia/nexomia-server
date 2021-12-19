dc8c5d2dd6e4730cbabec21a02838938d59e25f6
devdmytro <dmytro@d3s.ru>
2021-12-19 14:59:19 +0700

    fix: empty messages

--- src/api/channels/channels.service.ts
+++ src/api/channels/channels.service.ts
@@ -388,7 +388,7 @@ export class ChannelsService {
       })
       if (!user) throw new ForbiddenException()
       message.sticker_id = messageDto.sticker_id
-    } else if (message.content) {
+    } else if (messageDto.content) {
       message.content = messageDto.content?.trim()
       const emojisMatch = Array.from(
         message.content.matchAll(/(<e:)([0-9]{1,})(>)/g),
